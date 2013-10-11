d5e7358285fe40cd9b81202c7cabe5f27e653af2
loos <louisknapp@gmail.com>
2013-10-11 10:45:58 -0500

    Intialized Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubyboo::Application.config.secret_key_base = 'f0a9e40e8c0833019e085314e9a29b5194ba23ee703b2f90e2e92f98049f44fd8ae7f6ae8b0153345c132ca255e78669a337fc57d4e2632d3c2f38c019991bf3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rubyboo::Application.config.session_store :cookie_store, key: '_rubyboo_session'
